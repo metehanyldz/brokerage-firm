655dba481d0990b124fa2d3fbda380860d1ceb53
metehan yıldız <metehanyldz@gmail.com>
2024-09-11 11:59:54 +0300

    Necessary constants implemented: Side enum and Status enum

---
+++ src/main/java/com/ing/brokage/brokage_firm/constants/Side.java
@@ -0,0 +1,6 @@
+package com.ing.brokage.brokage_firm.constants;
+
+public enum Side {
+    BUY,
+    SELL;
+}
---
+++ src/main/java/com/ing/brokage/brokage_firm/constants/Status.java
@@ -0,0 +1,7 @@
+package com.ing.brokage.brokage_firm.constants;
+
+public enum Status {
+    PENDING,
+    MATCHED,
+    CANCELLED;
+}
