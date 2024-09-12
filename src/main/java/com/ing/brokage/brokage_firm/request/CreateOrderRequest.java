2c8a53439577b8e8f916e98e731707edfc10a5bb
metehan yıldız <metehanyldz@gmail.com>
2024-09-12 14:32:09 +0300

    CreateOrderRequest and DeleteOrderRequest implemented to satisfy OrderController request response needs.

---
+++ src/main/java/com/ing/brokage/brokage_firm/request/CreateOrderRequest.java
@@ -0,0 +1,45 @@
+package com.ing.brokage.brokage_firm.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ing.brokage.brokage_firm.constants.Side;
+import com.ing.brokage.brokage_firm.constants.Status;
+import com.ing.brokage.brokage_firm.model.Order;
+import com.ing.brokage.brokage_firm.model.OrderId;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+public class CreateOrderRequest {
+    private String customerId;
+
+    private String assetName;
+
+    private Side orderSide;
+
+    private BigDecimal size;
+
+    private BigDecimal price;
+
+    @JsonIgnore
+    public BigDecimal getTotalPrice() {
+        return this.getSize().multiply(this.getPrice());
+    }
+
+    @JsonIgnore
+    public Order toOrder() {
+        return Order.builder()
+                .orderId(new OrderId(UUID.randomUUID().toString(), this.getCustomerId(), this.getAssetName()))
+                .orderSide(this.orderSide)
+                .size(this.size)
+                .price(this.price)
+                .status(Status.PENDING)
+                .build();
+    }
+}
