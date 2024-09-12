7b33159fc1187ec279b6f5d5dbd75759d55f5fa0
metehan yıldız <metehanyldz@gmail.com>
2024-09-12 14:35:19 +0300

    CustomerController signup early implementation without jwt tokenb or security implementation

---
+++ src/main/java/com/ing/brokage/brokage_firm/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package com.ing.brokage.brokage_firm.controller;
+
+import com.ing.brokage.brokage_firm.model.Customer;
+import com.ing.brokage.brokage_firm.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    //signupCustomer
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping("signup")
+    public ResponseEntity<Customer> signup(@RequestParam String username,
+                                           @RequestParam String password) {
+        return ResponseEntity.ok(customerService.signupCustomer(username, password));
+    }
+}
